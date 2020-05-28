f2b22036b438fa946178a0275c9ad10cfd8d96b9
NileshBarandwal <nbarandwal@gmail.com>
2020-05-28 17:47:22 +0530

    heroku

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k*7ohj$1dcduu9w80o@mj3y&(0ok#j%7807(0$^xy!v&%!qxz1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sniperapi.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
